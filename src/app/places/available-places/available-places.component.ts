5b41124e9ddcd4a8871bd50c637c95daf4ed6e09
bansheett <avecesare01@gmail.com>
2025-05-07 17:26:31 +0200

    second commit

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,6 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -14,21 +15,47 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AvailablePlacesComponent implements OnInit{
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal('');
   private httpClient = inject(HttpClient);
-  private destroyRef = inject(DestroyRef)
+  private destroyRef = inject(DestroyRef);
 
   //constructor(private httpClient: HttpClient) {}
 
   ngOnInit() {
+    this.isFetching.set(true);
     const subscription = this.httpClient
-    .get<{ places: Place[] }>('http://localhost:3000/places').subscribe({
-      next: (resData) => {
-        console.log(resData.places);
+    .get<{ places: Place[] }>('http://localhost:3000/places')
+    .pipe(
+      map((resData) => resData.places),
+      catchError((error) => {
+        console.log(error);
+        return throwError(() => new Error('Something went wrong fetching the available places. Please try again later.'));
+      })
+    )
+    .subscribe({
+      next: (places) => {
+        this.places.set(places);
       },
+      error: (error: Error) => {
+        this.error.set(error.message);
+      },
+      complete: () => {
+        this.isFetching.set(false);
+      }
     });
-
     this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
+      if (subscription instanceof Subscription) {
+        subscription.unsubscribe();
+      }
+    });
+  }
+
+  onSelectPlace(selectedPlace: Place) {
+    this.httpClient.put('http://localhost:3000/user-places', {
+      placeId: selectedPlace.id,
+    }).subscribe({
+      next: (resData) => console.log(resData),
     });
   }
 }
